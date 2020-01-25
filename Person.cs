af070de57f239b1454b9fad21f81576fd66e53a1
ProtSenpai <jdjimenez04@hotmail.com>
2020-01-25 09:16:08 -0600

    try to understand Visulal C#, and the class person created with basics methods

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formulario
+{
+    public class Person
+    {
+        private String name;
+        private String lastName;
+        private int age;
+        private int semester;
+        private String career; 
+
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public String Age { get; set; }
+        public String Semester { get; set; }
+        public String Career { get; set; }
+
+    }
+}
